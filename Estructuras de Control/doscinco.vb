776b7d9cc05cd0f4179ccb742e9a2e238e195886
GonzLeandro <joayugi@gmail.com>
2018-05-14 15:36:24 -0300

    Actualizar

---
+++ Estructuras de Control/doscinco.vb
@@ -0,0 +1,52 @@
+﻿Module doscinco
+    Enum meses As Integer
+        enero
+        febrero
+        marzo
+        abril
+        mayo
+        junio
+        julio
+        agosto
+        septiembre
+        octubre
+        noviembre
+        diciembre
+
+    End Enum
+
+    Sub main()
+        Console.WriteLine("Ingrese un numero entre el 1 y el 12 segun su correspondiente mes: ")
+        Dim mes As Integer = Console.ReadLine()
+
+        Select Case mes
+            Case 1
+                Console.WriteLine(meses.enero.ToString)
+            Case 2
+                Console.WriteLine(meses.febrero.ToString)
+            Case 3
+                Console.WriteLine(meses.marzo.ToString)
+            Case 4
+                Console.WriteLine(meses.abril.ToString)
+            Case 5
+                Console.WriteLine(meses.mayo.ToString)
+            Case 6
+                Console.WriteLine(meses.junio.ToString)
+            Case 7
+                Console.WriteLine(meses.julio.ToString)
+            Case 8
+                Console.WriteLine(meses.agosto.ToString)
+            Case 9
+                Console.WriteLine(meses.septiembre.ToString)
+            Case 10
+                Console.WriteLine(meses.octubre.ToString)
+            Case 11
+                Console.WriteLine(meses.noviembre.ToString)
+            Case 12
+                Console.WriteLine(meses.diciembre.ToString)
+            Case Else
+                Console.WriteLine("Invalido")
+        End Select
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ Estructuras de Control/dosseis.vb
@@ -0,0 +1,26 @@
+﻿Module dosseis
+    Sub Main()
+        Dim desuento As Single
+        Dim precio, cant, subtotal As Integer
+        Console.WriteLine("Cantidad:")
+        cant = Console.ReadLine()
+        Console.WriteLine("Precio unitario:")
+        precio = Console.ReadLine()
+        Select Case cant
+            Case 10 To 50
+                descuento = 0.05
+            Case 51 To 250
+                descuento = 0.1
+            Case Is > 251
+                descuento = 0.2
+            Case Else
+                descuento = 0%
+        End Select
+        subtotal = cant * precio
+        Console.WriteLine("El porcentaje de descuento aplicado es del:  " & descuento * 100 & "%")
+        Console.WriteLine("Subtotal: " & subtotal)
+        Console.WriteLine("Monto descontado: {0} ", subtotal * descuento)
+        Console.WriteLine("Total a pagar: {0} ", subtotal - (subtotal * descuento))
+        Console.ReadKey()
+    End Sub
+End Module
