09848183676056fd22fd9f467f607f705f2aacee
GonzLeandro <joayugi@gmail.com>
2018-05-16 22:00:10 -0300

    a

---
+++ Estructuras de Control/doscatorce.vb
@@ -0,0 +1,40 @@
+﻿Module doscatorce
+    Sub main()
+        Dim cant As Int16
+        Dim pre, total As Single
+
+        Console.WriteLine("Ingrese la Cantidad del producto: ")
+        cant = Console.ReadLine()
+        Console.WriteLine("Ingrese el Precio unitario del producto: ")
+        pre = Console.ReadLine()
+
+        If cant >= 10 And cant <= 50 Then
+            total = (cant * pre) - (cant * pre) * 0.05
+            Console.WriteLine("El precio de cada unidad es: {0}$", pre)
+            Console.WriteLine("El subtotal es de: {0}", cant * pre)
+            Console.WriteLine("El descuento aplicado es de: {0}$", (cant * pre) * 0.05)
+            Console.WriteLine("El precio total es: {0}$", total)
+
+        ElseIf cant >= 51 And cant <= 250 Then
+            total = (cant * pre) - (cant * pre) * 0.1
+            Console.WriteLine("El precio de cada unidad es: {0}$", pre)
+            Console.WriteLine("El subtotal es de: {0}", cant * pre)
+            Console.WriteLine("El descuento aplicado es de: {0}$", (cant * pre) * 0.1)
+            Console.WriteLine("El precio total es: {0}$", total)
+        ElseIf cant >= 251 Then
+            total = (cant * pre) - (cant * pre) * 0.2
+            Console.WriteLine("El precio de cada unidad es: {0}$", pre)
+            Console.WriteLine("El subtotal es de: {0}", cant * pre)
+            Console.WriteLine("El descuento aplicado es de: {0}$", (cant * pre) * 0.2)
+            Console.WriteLine("El precio total es: {0}$", total)
+        Else
+            total = (cant * pre)
+            Console.WriteLine("El precio de cada unidad es: {0}$", pre)
+            Console.WriteLine("El subtotal es de: {0}", cant * pre)
+            Console.WriteLine("El precio total es: " & total)
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+End Module
---
+++ Estructuras de Control/dostrece.vb
@@ -0,0 +1,20 @@
+﻿Module dostrece
+    Sub main()
+        Dim n, i As Integer
+        Dim b As Boolean
+        For n = 1 To 1000
+            b = True
+            For i = 2 To Math.Sqrt(n)
+                If n Mod i = 0 Then
+                    b = False
+                    Exit For
+                End If
+            Next
+            If b And n <> 1 Then
+                Console.WriteLine(n)
+            End If
+        Next
+        Console.ReadKey()
+    End Sub
+End Module
+
